8e3a294ffd8d747f5ec769910706cf5245ea301b
Jonathan <jonathanman2010@icloud.com>
2019-10-03 11:50:40 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ContactDetailComponent,
+    ContactItemComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule
